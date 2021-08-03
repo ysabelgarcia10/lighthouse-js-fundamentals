3083907283825333a77d0eea351d7d92f1f7c4b0
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-03 18:04:39 +0000

    module 4 assignment: odd checker

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd = function(x) {
+  return x % 2 != 0;
+}
+
+console.log(isOdd(12));
+console.log(isOdd(100));
+console.log(isOdd(1003));
+console.log(isOdd(1002.5));
+console.log(isOdd(1));
+console.log(isOdd(0));
